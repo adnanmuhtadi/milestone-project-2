d7f5ae53a93c3559b2add39d05a00ba1bc7ff726
Adnan Muhtadi <adnanmuhtadi@hotmail.com>
2021-01-16 15:21:03 +0000

    utilised the scripted from emailjs

--- assets/js/sendEmail.js
+++ assets/js/sendEmail.js
@@ -1,17 +1,33 @@
-function sendMail(contactForm) {
-    emailjs.send("personal_gmail", "middle_east", {
-        "from_fullname": contactForm.fullname.value,
-        "from_useremail": contactForm.useremail.value,
-        "country-request": contactForm.country_request.value,
-        "question_asked": contactForm.question_asked.value
-    })
-        .then(
-        function(response) {
-            console.log("SUCCESS", response);
-        },
-        function(error) {
-            console.log("FAILED", error);
-        }
-    );
-    return false;  // To block from loading a new page
+let btn = document.getElementById('buttons');
+
+function changeSendBtnText() {
+    btn.textContent = 'Send';
 }
+
+document.getElementById('contactForm')
+    .addEventListener('submit', function (event) {
+    event.preventDefault();
+
+    btn.textContent = 'Sending...';
+
+    let serviceID = 'personal_gmail';
+    let templateID = 'middle_east';
+
+    emailjs.send(serviceID, templateID, {
+        "from_fullname": this.from_fullname.value,
+        "from_useremail": this.from_useremail.value,
+        "country_requested": this.country_requested.value,
+        "question_asked": this.question_asked.value,
+
+    })
+        .then((response) => {
+        btn.textContent = 'Sent';
+        console.log("Message Sent");
+        alert('success');
+
+    }, (error) => {
+        btn.textContent = 'Error';
+        console.log("Message Did Not Sent", error);
+        alert(`failed`);
+    });
+});
