151c74fe201533722ad2c6f01519aef356345fd5
Adnan Muhtadi <adnanmuhtadi@hotmail.com>
2021-01-25 10:22:13 +0000

    The html which would construct the content for the top places and foods in the said countries

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,3 +15,63 @@ for (let i = 1; i <= topRated; i++) {
     //This defines the div elemenet with the description to dynamically change.
     let cDescription = document.createElement("div");
     cDescription.innerHTML = '<div class="country-description" id="cDescription' + i + '"></div>';
+
+    //Within this div element, inside the div element is all the html which would contruct my content.
+    let topRatedCon = document.createElement("div");
+    topRatedCon.innerHTML = `<div class="row top-rated-container">
+<div class="col-6 left-col">
+<h2 id="top-places">Top Rated Places</h2>
+<div class="row top-rated-places" id="top-rated-place1-${i}">
+<img id="place-img1-${i}" src="" class="col-4 place-image">
+<div class="col-8 place-info">
+<h4 id="place-name1-${i}"></h4>
+<p id="place-desc1-${i}"></p>
+<a href="" id="place-more1-${i}" class="more-link" target="_blank">Learn More!</a>
+</div>
+</div>
+<div class="row top-rated-places" id="top-rated-place2-${i}">
+<img id="place-img2-${i}" src="" class="col-4 place-image">
+<div class="col-8 place-info">
+<h4 id="place-name2-${i}"></h4>
+<p id="place-desc2-${i}"></p>
+<a href="" id="place-more2-${i}" class="more-link" target="_blank">Learn More!</a>
+</div>
+</div>
+<div class="row top-rated-places" id="top-rated-place3-${i}">
+<img id="place-img3-${i}" src="" class="col-4 place-image">
+<div class="col-8 place-info">
+<h4 id="place-name3-${i}"></h4>
+<p id="place-desc3-${i}"></p>
+<a href="" id="place-more3-${i}" class="more-link" target="_blank">Learn More!</a>
+</div>
+</div>
+</div>
+<div class="col-6 right-col">
+<h2 id="top-food">Top Rated Cuisines</h2>
+<div class="row top-rated-foods" id="top-rated-food1-${i}">
+<img id="food-img1-${i}" src="" class="col-4 food-image">
+<div class="col-8 food-info">
+<h4 id="food-name1-${i}"></h4>
+<p id="food-desc1-${i}"></p>
+<a href="" id="food-more1-${i}" class="more-link" target="_blank">Learn More!</a>
+</div>
+</div>
+<div class="row top-rated-foods" id="top-rated-food2-${i}">
+<img id="food-img2-${i}" src="" class="col-4 food-image" >
+<div class="col-8 food-info">
+<h4 id="food-name2-${i}"></h4>
+<p id="food-desc2-${i}"></p>
+<a href="" id="food-more2-${i}" class="more-link" target="_blank">Learn More!</a>
+</div>
+</div>
+<div class="row top-rated-foods" id="top-rated-food3-${i}">
+<img id="food-img3-${i}" src="" class="col-4 food-image" >
+<div class="col-8 food-info">
+<h4 id="food-name3-${i}"></h4>
+<p id="food-desc3-${i}"></p>
+<a href="" id="food-more3-${i}" class="more-link" target="_blank">Learn More!</a>
+</div>
+</div>
+</div>
+</div>`;
+
